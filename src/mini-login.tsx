728c97965c944d242547ea4612c287b5ed789c55
Kaue Oliveira <elmovckaue@gmail.com>
2021-04-10 06:02:46 +0000

    Convert to typescript and begin setting layout

---
+++ src/mini-login.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './mini-login.css';
+
+class MiniLogin extends Component {
+	render() {
+		return (
+			<nav className='mini-login'>
+				<div className='user-field-block'>
+                    <p>Username</p>
+					<input className='user-field' type='text' />
+				</div>
+                <div className='password-field-block'>
+                    <p>Password</p>
+					<input className='password-field' type='text' />
+				</div>
+                <button className='submit' children='submit' />
+			</nav>
+		);
+	}
+}
+
+export default MiniLogin;
