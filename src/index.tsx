d88690d4c2c3e3b038eadaa6803f920404f1881c
Kaue Oliveira <elmovckaue@gmail.com>
2021-04-12 00:15:18 +0000

    Touch up hamburger and format files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Root from "./components/root";
+
+import "./index.scss";
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
