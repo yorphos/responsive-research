7255c4657c63f2011b9d9d7bb7585b53f60cd054
Kaue Oliveira <elmovckaue@gmail.com>
2021-04-12 17:59:58 +0000

    Add scrolling to main and side contents, tweak side menu and hamburger positioning

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+
 import './siderbar.scss';
 
 type SideBarProps = {
@@ -7,7 +10,14 @@ type SideBarProps = {
 const SideBar = (props: SideBarProps) => {
 	switch (props.isOpen) {
 		case false:
-			return <div className="sidebar-mini">te</div>;
+			return (
+				<div className="sidebar-mini">
+					<div className="mini-icon-stack">
+						<FontAwesomeIcon icon={faHome} />
+						<FontAwesomeIcon icon={faNewspaper} />
+					</div>
+				</div>
+			);
 		case true:
 			return <div className="sidebar-extended">test!</div>;
 	}
